8263a8e824460455d9698458b5bc3bc9a47e0621
Maurdekye <ncolaprete@gmail.com>
2024-08-10 00:51:03 +0300

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,404 @@
+#![feature(int_roundings)]
+#![feature(generic_arg_infer)]
+#![feature(array_chunks)]
+use std::{
+    fmt::Display,
+    ops::{Index, IndexMut},
+    str::FromStr,
+};
+
+use space_search::{search::guided, Scoreable, Searchable, Searcher, SolutionIdentifiable};
+
+#[derive(Clone, Hash, PartialEq, Eq)]
+struct Board<Cell>([Cell; 81]);
+
+impl<Cell> Board<Cell> {
+    fn iter(&self) -> impl Iterator<Item = &Cell> {
+        self.0.iter()
+    }
+
+    fn iter_mut(&mut self) -> impl Iterator<Item = &mut Cell> {
+        self.0.iter_mut()
+    }
+}
+
+impl<Cell> Index<(usize, usize)> for Board<Cell> {
+    type Output = Cell;
+
+    fn index(&self, (x, y): (usize, usize)) -> &Self::Output {
+        &self.0[y * 9 + x]
+    }
+}
+
+impl<Cell> IndexMut<(usize, usize)> for Board<Cell> {
+    fn index_mut(&mut self, (x, y): (usize, usize)) -> &mut Self::Output {
+        &mut self.0[y * 9 + x]
+    }
+}
+
+#[derive(Clone, Copy, Hash, PartialEq, Eq)]
+enum Space {
+    One,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+}
+
+impl Space {
+    fn idx(&self) -> usize {
+        let space_number: usize = (*self).into();
+        space_number - 1
+    }
+}
+
+impl From<Space> for usize {
+    fn from(value: Space) -> Self {
+        use Space::*;
+        match value {
+            One => 1,
+            Two => 2,
+            Three => 3,
+            Four => 4,
+            Five => 5,
+            Six => 6,
+            Seven => 7,
+            Eight => 8,
+            Nine => 9,
+        }
+    }
+}
+
+impl TryFrom<usize> for Space {
+    type Error = String;
+
+    fn try_from(value: usize) -> Result<Self, Self::Error> {
+        use Space::*;
+        let result = match value {
+            1 => One,
+            2 => Two,
+            3 => Three,
+            4 => Four,
+            5 => Five,
+            6 => Six,
+            7 => Seven,
+            8 => Eight,
+            9 => Nine,
+            _ => return Err(format!("Cant convert '{}' to a space value", value)),
+        };
+        Ok(result)
+    }
+}
+
+type SudokuBoard = Board<Option<Space>>;
+type PossibilitySpaceBoard = Board<[bool; 9]>;
+
+impl SudokuBoard {
+    fn reduce(&mut self) -> PossibilitySpaceBoard {
+        // prepare possibility space
+        let mut possibility_space = Board([[true; 9]; _]);
+        for (possibilities, space) in possibility_space.iter_mut().zip(self.iter()) {
+            if let Some(space) = space {
+                let space_number = space.idx();
+                for (space_idx, value) in possibilities.iter_mut().enumerate() {
+                    *value = space_number == space_idx;
+                }
+            }
+        }
+
+        loop {
+            let mut adjusted = false;
+
+            for i in 0..81 {
+                let (x, y) = (i % 9, i / 9);
+                let mut new_possibilities = possibility_space[(x, y)].clone();
+
+                // check current row
+                for x in 0..9 {
+                    if let Some(space) = &self[(x, y)] {
+                        new_possibilities[space.idx()] = false;
+                    }
+                }
+
+                // check current column
+                for y in 0..9 {
+                    if let Some(space) = &self[(x, y)] {
+                        new_possibilities[space.idx()] = false;
+                    }
+                }
+
+                // check current box
+                let left = x - x % 3;
+                let top = y - y % 3;
+                for i in 0..9 {
+                    let (x, y) = (left + (i % 3), top + (i / 3));
+                    if let Some(space) = &self[(x, y)] {
+                        new_possibilities[space.idx()] = false;
+                    }
+                }
+
+                // update possibility space
+                if new_possibilities != possibility_space[(x, y)] {
+                    adjusted = true;
+                }
+                possibility_space[(x, y)] = new_possibilities;
+
+                // confirm square if all alternative possibilities are exhausted
+                if let &[(i, _)] = &new_possibilities
+                    .iter()
+                    .enumerate()
+                    .filter(|(_, p)| **p)
+                    .collect::<Vec<_>>()[..]
+                {
+                    self[(x, y)] = Some(
+                        (i + 1)
+                            .try_into()
+                            .expect("index will always correspond to a valid space value"),
+                    );
+                }
+            }
+
+            if !adjusted {
+                break;
+            }
+        }
+
+        possibility_space
+    }
+}
+
+impl FromStr for SudokuBoard {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let collect = s
+            .split('\n')
+            .flat_map(|row| {
+                row.chars()
+                    .map(|chr| match chr {
+                        ' ' => Ok(None),
+                        '1'..='9' => Ok(Some(
+                            Space::try_from(
+                                chr.to_digit(10)
+                                    .expect("char will always be convertible to a digit")
+                                    as usize,
+                            )
+                            .expect("char will always be convertible to a digit"),
+                        )),
+                        _ => Err(format!(
+                            "Character '{}' is not valid for a sudoku board",
+                            chr
+                        )),
+                    })
+                    .collect::<Result<Vec<_>, _>>()
+            })
+            .flatten()
+            .collect::<Vec<_>>();
+        Ok(Board(collect.try_into().map_err(|_| {
+            String::from("Incorrect number of spaces on sudoku boad")
+        })?))
+    }
+}
+
+impl Display for SudokuBoard {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            self.0
+                .array_chunks()
+                .map(|row: &[_; 9]| {
+                    row.iter()
+                        .map(|space| match space {
+                            None => String::from(" "),
+                            Some(space) => format!("{}", usize::from(*space)),
+                        })
+                        .collect::<Vec<_>>()
+                        .join("")
+                })
+                .collect::<Vec<_>>()
+                .join("\n")
+        )
+    }
+}
+
+struct NextSudokuBoardsIterator {
+    reduced_board: SudokuBoard,
+    possibilities_board: PossibilitySpaceBoard,
+    index: usize,
+    sub_index: usize,
+}
+// static mut COUNTER: usize = 0;
+
+impl Iterator for NextSudokuBoardsIterator {
+    type Item = SudokuBoard;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            let NextSudokuBoardsIterator {
+                reduced_board,
+                possibilities_board,
+                index,
+                sub_index,
+            } = self;
+
+            // unsafe {
+            //     if COUNTER % 1_000_000 == 0 {
+            //         println!("---------\n{reduced_board}");
+            //     }
+            //     COUNTER += 1;
+            // }
+
+            if *index >= 81 {
+                return None;
+            }
+
+            // hacky but im too lazy to implement this properly with a de facto abstraction
+            if reduced_board.is_solution() {
+                *index = 81;
+                return Some(self.reduced_board.clone());
+            }
+
+            if *sub_index >= 9 {
+                *index += 1;
+                *sub_index = 0;
+                continue;
+            }
+
+            let (x, y) = (*index % 9, *index / 9);
+
+            if reduced_board[(x, y)].is_some() {
+                *index += 1;
+                continue;
+            }
+
+            let possibilities = &possibilities_board[(x, y)];
+
+            if possibilities[*sub_index] {
+                let mut new_board = reduced_board.clone();
+                new_board[(x, y)] = Some(Space::try_from(*sub_index + 1).unwrap());
+                *sub_index += 1;
+                return Some(new_board);
+            }
+
+            *sub_index += 1;
+        }
+    }
+}
+
+impl Searchable for SudokuBoard {
+    fn next_states(&self) -> impl Iterator<Item = Self> {
+        let mut reduced_board = self.clone();
+        let possibilities_board = reduced_board.reduce();
+        return NextSudokuBoardsIterator {
+            reduced_board,
+            possibilities_board,
+            index: 0,
+            sub_index: 0,
+        };
+    }
+}
+
+impl SolutionIdentifiable for SudokuBoard {
+    fn is_solution(&self) -> bool {
+        self.iter().all(|space| space.is_some())
+    }
+}
+
+impl Scoreable for SudokuBoard {
+    type Score = usize;
+
+    fn score(&self) -> Self::Score {
+        self.iter().filter(|space| space.is_none()).count()
+    }
+}
+
+#[test]
+fn test_reduction() {
+    #[rustfmt::skip]
+    let board_str = 
+"53  7    
+6  195   
+ 98    6 
+8   6   3
+4  8 3  1
+7   2   6
+ 6    28 
+   419  5
+    8  79";
+    let mut board: SudokuBoard = board_str.parse().unwrap();
+    println!("initial board:");
+    println!("{}", board);
+    board.reduce();
+    println!("after reduction:");
+    println!("{}", board);
+}
+
+#[test]
+fn test_solve_hard() {
+    #[rustfmt::skip]
+    let board_str = 
+"2  5 74 6
+    31   
+      23 
+    2    
+86 31    
+ 45      
+  9   7  
+  695   2
+  1  6  8";
+    let board: SudokuBoard = board_str.parse().unwrap();
+    println!("initial board:");
+    println!("{}", board);
+    let mut searcher: Searcher<guided::no_route::hashable::Manager<_>, _> = Searcher::new(board);
+    let solution = searcher.next().expect("Sudoku board has a solution");
+    println!("solution:");
+    println!("{}", solution);
+}
+
+#[test]
+fn test_solve_hard_2() {
+    #[rustfmt::skip]
+    let board_str = 
+"  65     
+7 5  23  
+ 3     8 
+ 5  96 7 
+1 4     8
+   82    
+ 2     9 
+  72  4  
+     75  ";
+    let board: SudokuBoard = board_str.parse().unwrap();
+    println!("initial board:");
+    println!("{}", board);
+    let mut searcher: Searcher<guided::no_route::hashable::Manager<_>, _> = Searcher::new(board);
+    let solution = searcher.next().expect("Sudoku board has a solution");
+    println!("solution:");
+    println!("{}", solution);
+}
+
+fn main() {
+    #[rustfmt::skip]
+    let board_str = 
+"2  5 74 6
+    31   
+      23 
+    2    
+86 31    
+ 45      
+  9   7  
+  695   2
+  1  6  8";
+    let board: SudokuBoard = board_str.parse().unwrap();
+    println!("initial board:");
+    println!("{}", board);
+    let mut searcher: Searcher<guided::no_route::hashable::Manager<_>, _> = Searcher::new(board);
+    let solution = searcher.next().expect("Sudoku board has a solution");
+    println!("solution:");
+    println!("{}", solution);
+}
